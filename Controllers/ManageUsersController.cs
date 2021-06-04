3226002bba3c829e287f8d4fa10a759177bddf4d
Elahn <lightninglioness60@gmail.com>
2021-06-04 10:08:18 -0700

    Added Manage Users controller and Manage Users view.

---
+++ Controllers/ManageUsersController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTodo.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ManageUsersController(
+            UserManager<ApplicationUser> userManager)
+            {
+                _userManager = userManager;
+            }
+        public async Task<IActionResult> Index()
+        {
+            var admins = (await _userManager.GetUsersInRoleAsync("Administrator")).ToArray();
+            var everyone = await _userManager.Users.ToArrayAsync();
+            var model = new ManageUsersViewModel
+            {
+                Administrators = admins,
+                Everyone = everyone
+            };
+            return View(model);
+        }
+    }
+}
